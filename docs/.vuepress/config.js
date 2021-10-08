0383aeecfb7cf7c4c2a18f82b7351e7a7c0ae84d
“zhangxiangyu” <zhangxiangyu@zeaho.com>
2021-10-08 20:48:12 +0800

    📖

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,253 +1,285 @@
 // const moment = require("moment");
 
 module.exports = {
-  title: "大前端工程师成长路线",
-  description: "博客 分享 笔记 规划 ",
-  base: "/docs/",
-  port: "3000",
-  dest: "./dist",
-  lastUpdated: "上次更新时间",
-  // theme: '@vuepress/blog',
+    title: "大前端工程师成长路线",
+    description: "博客 分享 笔记 规划 ",
+    base: "/docs/",
+    port: "3000",
+    dest: "./dist",
+    lastUpdated: "上次更新时间",
+    // theme: '@vuepress/blog',
 
-  // markdown: {
-  //   anchor: { permalink: false },
-  //   toc: { includeLevel: [1, 2, 3] },
-  //   config: (md) => {
-  //     md.use(require("markdown-it-include"), "./");
-  //   },
-  // },
-  plugins: [
-    '@vuepress/pwa', {
-      serviceWorker: true,
-      updatePopup: true
-    },
-    ["@vuepress/back-to-top"], // 返回顶部
-    ["@vuepress/nprogress"],   // 加载进度条 
-  ],
-  themeConfig: {
-    logo: '/images/home/balu.jpg',
-    // navbar: true,
-    // displayAllHeaders: true, // 默认值：false
-
-    nav: [
-      { text: 'Home', link: '/' },
-      {
-        text: '移动端',
-        items: [
-          { text: "App", link:"/App/App/"},
-          { text: "桌面应用", link:"/App/Desktop/"},
-        ]
-      },
-      {
-        text: 'Web端',
-        items: [
-          { text: "JavaScript", link:"/Web/JavaScript/"},
-          { text: "HTML", link:"/Web/HTML/"},
-          { text: "CSS", link: "/Web/CSS/" },
-        ]
-      },
-      {
-        text: 'React系列',
-        items: [
-          { text: "AntDesign", link:"/React/AntDesign/"},
-          { text: "React入门", link:"/React/ReactPrimer/"},
-        ]
-      },
-      { 
-        text: 'Node',
-        items: [
-          { text: "Node", link: "/Node/" },
-          { text: "Nest框架", link: "/Node/Nest/" },
-        ]
-      },
-      {
-        text: '打包与工具链',
-        items: [
-          { text: "Server", link: "/Tools/Server/" },
-          { text: "Webpack", link: "/Tools/Webpack/" },
-        ]
-      },
-      {
-        text: '其他',
-        items: [
-          { text: "面试题", link: "/Others/Question/" },
-          { text: "区块链", link: "/Others/BlockChain/" }
-        ]
-      },
-      { text: 'GitHub', link: 'https://github.com/bierxiensi' },
+    // markdown: {
+    //   anchor: { permalink: false },
+    //   toc: { includeLevel: [1, 2, 3] },
+    //   config: (md) => {
+    //     md.use(require("markdown-it-include"), "./");
+    //   },
+    // },
+    plugins: [
+        "@vuepress/pwa",
+        {
+            serviceWorker: true,
+            updatePopup: true,
+        },
+        ["@vuepress/back-to-top"], // 返回顶部
+        ["@vuepress/nprogress"], // 加载进度条
     ],
+    themeConfig: {
+        logo: "/images/home/balu.jpg",
+        // navbar: true,
+        // displayAllHeaders: true, // 默认值：false
 
-    sidebar: {
-      "/all/": [
-        {
-          title: '这里是所有的路由',   // 必要的
-          collapsable: false, // 可选的, 默认值是 true,
+        nav: [
+            { text: "Home", link: "/" },
+            {
+                text: "移动端",
+                items: [
+                    { text: "App", link: "/App/App/" },
+                    { text: "桌面应用", link: "/App/Desktop/" },
+                ],
+            },
+            {
+                text: "Web端",
+                items: [
+                    { text: "JavaScript", link: "/Web/JavaScript/" },
+                    { text: "HTML", link: "/Web/HTML/" },
+                    { text: "CSS", link: "/Web/CSS/" },
+                ],
+            },
+            {
+                text: "React系列",
+                items: [
+                    { text: "AntDesign", link: "/React/AntDesign/" },
+                    { text: "React入门", link: "/React/ReactPrimer/" },
+                ],
+            },
+            {
+                text: "Node",
+                items: [
+                    { text: "Node", link: "/Node/" },
+                    { text: "Nest框架", link: "/Node/Nest/" },
+                ],
+            },
+            {
+                text: "打包与工具链",
+                items: [
+                    { text: "Server", link: "/Tools/Server/" },
+                    { text: "Webpack", link: "/Tools/Webpack/" },
+                ],
+            },
+            {
+                text: "其他",
+                items: [
+                    { text: "面试题", link: "/Others/Question/" },
+                    { text: "区块链", link: "/Others/BlockChain/" },
+                    { text: "学习资源", link: "/Others/StudySource/" },
+                ],
+            },
+            { text: "GitHub", link: "https://github.com/bierxiensi" },
+        ],
+
+        sidebar: {
+            "/all/": [
+                {
+                    title: "这里是所有的路由", // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                },
+            ],
+
+            // app
+            "/App/App/": [
+                {
+                    title: "app", // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    children: [
+                        {
+                            title: "app",
+                            path: "/App/App/",
+                        },
+                        {
+                            title: "cordova入门",
+                            path: "/App/App/Cordova",
+                        },
+                    ],
+                },
+            ],
+            "/App/Desktop/": [
+                {
+                    title: "桌面应用", // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    children: [
+                        {
+                            title: "桌面应用",
+                            path: "/App/Desktop/",
+                        },
+                    ],
+                },
+            ],
+
+            // web
+            "/Web/JavaScript/": [
+                {
+                    title: "ES6 到 ES12 特性总结", // 必要的
+                    path: "/Web/JavaScript/",
+                },
+                {
+                    title: "JavaScript初级", // 必要的
+                    collapsable: true, // 可选的, 默认值是 true,
+                    children: [
+                        {
+                            title: "文本对象模型DOM",
+                            path: "/Web/JavaScript/DOM",
+                        },
+                        {
+                            title: "浏览器对象模型BOM",
+                            path: "/Web/JavaScript/BOM",
+                        },
+                        {
+                            title: "JavaScript中的this",
+                            path: "/Web/JavaScript/this",
+                        },
+                    ],
+                },
+            ],
+            "/Web/HTML/": [
+                {
+                    title: "HTML", // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    children: [
+                        {
+                            title: "HTML5",
+                            path: "/Web/HTML/html",
+                        },
+                    ],
+                },
+            ],
+            "/Web/CSS/": [
+                {
+                    title: "CSS", // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    children: [
+                        {
+                            title: "CSS目录",
+                            path: "/Web/CSS/",
+                        },
+                    ],
+                },
+            ],
+
+            // react
+            "/React/AntDesign/": [
+                { title: "AntDesign", path: "/React/AntDesign/" },
+            ],
+            "/React/ReactPrimer/": [
+                {
+                    title: "React入门",
+                    collapsable: false,
+                    children: [
+                        { title: "React与Vue", path: "/React/ReactPrimer/" },
+                        {
+                            title: "React入门（JSX）",
+                            path: "/React/ReactPrimer/JSX/",
+                        },
+                        {
+                            title: "React入门（一、React基础）",
+                            path: "/React/ReactPrimer/React_1",
+                        },
+                    ],
+                },
+            ],
+
+            // node
+            "/Node/": [
+                {
+                    title: "Node",
+                    collapsable: false,
+                    children: [{ title: "Node", path: "/Node/" }],
+                },
+            ],
+            "/Node/Nest/": [
+                {
+                    title: "Node",
+                    collapsable: false,
+                    children: [{ title: "Nest框架", path: "/Node/Nest" }],
+                },
+            ],
+
+            // Tools
+            "/Tools/Server/": [
+                {
+                    title: "Server端相关知识",
+                    collapsable: false,
+                    children: [
+                        {
+                            title: "防火墙linux",
+                            path: "/Tools/Server/linux",
+                        },
+                        {
+                            title: "nginx项目部署",
+                            path: "/Tools/Server/nginx",
+                        },
+                    ],
+                },
+            ],
+            "/Tools/Webpack/": [
+                {
+                    title: "Webpack相关知识",
+                    collapsable: false,
+                    children: [
+                        {
+                            title: "Webpack",
+                            path: "/Tools/Webpack/",
+                        },
+                    ],
+                },
+            ],
+
+            // others
+            "/Others/Question/": [
+                {
+                    title: "Question",
+                    collapsable: false,
+                    children: [
+                        {
+                            title: "前端面试题集锦",
+                            path: "/Others/Question/",
+                        },
+                    ],
+                },
+            ],
+            "/Others/BlockChain/": [
+                {
+                    title: "区块链",
+                    collapsable: false,
+                    children: [
+                        {
+                            title: "区块链基础知识",
+                            path: "/Others/BlockChain/",
+                        },
+                    ],
+                },
+            ],
+            "/Others/StudySource/": [
+                {
+                    title: "学习资源",
+                    collapsable: false,
+                    children: [
+                        {
+                            title: "Web端学习资源",
+                            path: "/Others/BlockChain/Web",
+                        },
+                        {
+                            title: "Vue学习资源",
+                            path: "/Others/BlockChain/Vue",
+                        },
+                        {
+                            title: "服务端学习资源",
+                            path: "/Others/BlockChain/Server",
+                        },
+                    ],
+                },
+            ],
         },
-      ],
-
-      // app
-      "/App/App/": [
-        {
-          title: 'app',   // 必要的
-          collapsable: false, // 可选的, 默认值是 true,
-          children: [
-            {
-              title: 'app',
-              path: '/App/App/',
-            },
-            {
-              title: 'cordova入门',
-              path: '/App/App/Cordova',
-            },
-          ]
-        }
-      ],
-      "/App/Desktop/": [
-        {
-          title: '桌面应用',   // 必要的
-          collapsable: false, // 可选的, 默认值是 true,
-          children: [
-            {
-              title: '桌面应用',
-              path: '/App/Desktop/',
-            },
-          ]
-        }
-      ],
-
-      // web
-      "/Web/JavaScript/": [
-        {
-          title: 'ES6 到 ES12 特性总结',   // 必要的
-          path: '/Web/JavaScript/',
-        },
-        {
-          title: 'JavaScript初级',   // 必要的
-          collapsable: true, // 可选的, 默认值是 true,
-          children: [     
-            {
-              title: '文本对象模型DOM',
-              path: '/Web/JavaScript/DOM',
-            },
-            {
-              title: '浏览器对象模型BOM',
-              path: '/Web/JavaScript/BOM',
-            },
-            {
-              title: 'JavaScript中的this',
-              path: '/Web/JavaScript/this',
-            },
-          ]
-        }
-      ],
-      "/Web/HTML/": [
-        {
-          title: 'HTML',   // 必要的
-          collapsable: false, // 可选的, 默认值是 true,
-          children: [
-            {
-              title: 'HTML5',
-              path: '/Web/HTML/html',
-            }
-          ]
-        }
-      ],
-      "/Web/CSS/": [
-        {
-          title: 'CSS',   // 必要的
-          collapsable: false, // 可选的, 默认值是 true,
-          children: [
-            {
-              title: 'CSS目录',
-              path: '/Web/CSS/',
-            }
-          ]
-        }
-      ],
-
-      // react
-      "/React/AntDesign/": [
-        { title: 'AntDesign', path: '/React/AntDesign/' }
-      ],
-      "/React/ReactPrimer/": [
-        {
-          title: 'React入门',
-          collapsable: false,
-          children: [
-            { title: 'React与Vue', path: '/React/ReactPrimer/' },
-            { title: 'React入门（JSX）', path: '/React/ReactPrimer/JSX/' },
-            { title: 'React入门（一、React基础）', path: '/React/ReactPrimer/React_1' }
-          ]
-        }
-      ],
-
-      // node
-      "/Node/": [
-        {
-          title: 'Node',
-          collapsable: false,
-          children: [
-            { title: 'Node', path: '/Node/' },
-          ]
-        }
-      ],
-      "/Node/Nest/": [
-        {
-          title: 'Node',
-          collapsable: false,
-          children: [
-            { title: 'Nest框架', path: '/Node/Nest' }
-          ]
-        }
-      ],
-
-      // Tools
-      "/Tools/Server/": [{
-        title: 'Server端相关知识',
-        collapsable: false,
-        children: [
-          {
-            title: '防火墙linux',
-            path: '/Tools/Server/linux',
-          },
-          {
-            title: 'nginx项目部署',
-            path: '/Tools/Server/nginx',
-          }
-        ]
-      }],
-      "/Tools/Webpack/": [{
-        title: 'Webpack相关知识',
-        collapsable: false,
-        children: [
-          {
-            title: 'Webpack',
-            path: '/Tools/Webpack/',
-          }
-        ]
-      }],
-
-
-      // others
-      "/Others/Question/": [{
-        title: 'Question',
-        collapsable: false,
-        children:[{
-          title: '前端面试题集锦',
-          path: '/Others/Question/',
-        }]
-      }],
-      "/Others/BlockChain/": [{
-        title: '区块链',
-        collapsable: false,
-        children: [
-          {
-            title: '区块链基础知识',
-            path: '/Others/BlockChain/',
-          }
-        ]
-      }],
-      
-    }
-  }
-}
+    },
+};
--- docs/React/ReactPrimer/README.md
+++ docs/React/ReactPrimer/README.md
@@ -9,160 +9,177 @@ description:
 comments:
 ---
 
+## 两个场景
+
 场景一：
+
 ```javascript
 [❌]this.state.Status === 'OK'
 [✔️]this.setState({ Status: 'OK' })
 ```
 
-## 本质 js库 VS js框架
-[官网简介React](https://reactjs.org/)
-> A <font color="red">JavaScript library</font> for building user interfaces
+# 一、本质 js 库 VS js 框架
 
-[官网简介Vue](https://reactjs.org/)
-> The Progressive <font color="red">JavaScript Framework</font>
+[官网简介 React](https://reactjs.org/)
 
-1\. [React] VS [Vue] = [js库] VS [js框架] <br>
-2\. Vue做的更多，React需要开发者做的更多
+> A <font color="yellow">JavaScript library</font> for building user interfaces
 
+[官网简介 Vue](https://reactjs.org/)
+
+> The Progressive <font color="yellow">JavaScript Framework</font>
+
+1\. [React] VS [Vue] = [js 库] VS [js 框架] <br>
+2\. Vue 做的更多，React 需要开发者做的更多
+
+# 二、差异对比
 
 ## 数据渲染 单向数据流 VS 双向数据流
-![avatar](./images/README(2).jpg)
 
-[React] 开发者通过setState更新state的值来达到重新render <br>
-[Vue] 响应式数据渲染，通过getter/setter(vue2.x)以及一些函数的劫持可以精确感知数据变化
+![avatar](<./images/README(2).jpg>)
 
-1\. Vue 和 React 设计理念上的区别，Vue 使用的是可变数据，而React(onChange/setState()模式)更强调数据的不可变
+[React] 开发者通过 setState 更新 state 的值来达到重新 render <br>
+[Vue] 响应式数据渲染，通过 getter/setter(vue2.x)以及一些函数的劫持可以精确感知数据变化
+
+1\. Vue 和 React 设计理念上的区别，Vue 使用的是可变数据，而 React(onChange/setState()模式)更强调数据的不可变 <br>
 2\. 由于一般会用 Vuex 以及 Redux 等单向数据流的状态管理框架，因此很多时候我们感受不到这一点区别
 
-
-
 ## API
-[Vue]Vue的template在处理上更加优雅于React的jsx
-[React]Class Component中在render中会存在大量porps, state的解构算是一个痛点
-Vue对template则不需要通过this.data.xxx来渲染options api
 
+[Vue]Vue 的 template 在处理上更加优雅于 React 的 jsx <br>
+[React]Class Component 中在 render 中会存在大量 porps, state 的解构算是一个痛点
+Vue 对 template 则不需要通过 this.data.xxx 来渲染 options api
 
-## 上手
-#### 脚手架
-[Vue]VueCli
-[React]
-[❌]create-react-app
-[✔️]umi => 蚂蚁团队ant design, dva
+## 脚手架
 
-#### 路由
+[Vue]VueCli <br>
+[React] <br>
+[❌]create-react-app <br>
+[✔️]umi => 蚂蚁团队 ant design, dva
+
+## 路由
+
 [Vue]
+
 ```javascript
 export default {
-  routes: [
-    { path: '/login', component: 'login' },
-    {
-      path: '/',
-      component: '@/layouts/index',
-      children: [
-        { path: '/list', component: 'list' },
-        { path: '/main', component: 'main' },
-      ],
-    }, 
-  ],
-}
+    routes: [
+        { path: "/login", component: "login" },
+        {
+            path: "/",
+            component: "@/layouts/index",
+            children: [
+                { path: "/list", component: "list" },
+                { path: "/main", component: "main" },
+            ],
+        },
+    ],
+};
 ```
+
 [React]
+
 ```javascript
 export default {
-  routes: [
-    { path: '/login', component: 'login' },
-    {
-      path: '/',
-      component: '@/layouts/index',
-      routes: [
-        { path: '/list', component: 'list' },
-        { path: '/main', component: 'main' },
-      ],
-    }, 
-  ],
-}
+    routes: [
+        { path: "/login", component: "login" },
+        {
+            path: "/",
+            component: "@/layouts/index",
+            routes: [
+                { path: "/list", component: "list" },
+                { path: "/main", component: "main" },
+            ],
+        },
+    ],
+};
 ```
 
-[对照umi]
-  | onRouteChange
+[对照 umi]
 
-  addRoutes | patchRoutes
-#### 状态管理
+|   属性   |  vue-router  | react-router  |      umi       |
+| :------: | :----------: | :-----------: | :------------: |
+| 路由变化 |      ｜      | onRouteChange |
+| 路由新增 | addRoutes ｜ | onRouteChange | patchRoutes ｜ |
+
+## 状态管理
+
 [Vue]vuex
+
 ```javascript
 export default {
-  // 同样的命名空间
-  namespace: 'todo',
-  state: {
-    // 类似Vuex State
-    list: []
-  },
-  Mutations: {
-    // 类似Vuex Mutations
-    save(state, { payload: { list } }) {
-      return { ...state, list }
-    }
-  },
-  actions: {
-    
-  }
-}
+    // 同样的命名空间
+    namespace: "todo",
+    state: {
+        // 类似Vuex State
+        list: [],
+    },
+    Mutations: {
+        // 类似Vuex Mutations
+        save(state, { payload: { list } }) {
+            return { ...state, list };
+        },
+    },
+    actions: {},
+};
 ```
-[React]redux全家桶 ,Mobx  ==> dva
+
+[React]redux 全家桶 ,Mobx ==> dva
+
 ```javascript
 export default {
-  // 同样的命名空间
-  namespace: 'todo',
-  state: {
-    // 类似Vuex State
-    list: []
-  },
-  reducers: {
-    // 类似Vuex Mutations
-    save(state, { payload: { list } }) {
-      return { ...state, list }
-    }
-  },
-  effects: {
-    *queryUserInfo({ payload: value }, { call, put, select }) {
-      // 对比Vuex Actions
-    }
-  },
-  subscriptions: {
-    setup({ dispatch, history }) {
-      // 监听路由的变化，请求页面数据
-    }
-  }
-}
+    // 同样的命名空间
+    namespace: "todo",
+    state: {
+        // 类似Vuex State
+        list: [],
+    },
+    reducers: {
+        // 类似Vuex Mutations
+        save(state, { payload: { list } }) {
+            return { ...state, list };
+        },
+    },
+    effects: {
+        *queryUserInfo({ payload: value }, { call, put, select }) {
+            // 对比Vuex Actions
+        },
+    },
+    subscriptions: {
+        setup({ dispatch, history }) {
+            // 监听路由的变化，请求页面数据
+        },
+    },
+};
 ```
 
-## 特殊的
-#### [Vue]插槽
-[vue]在Vue中通过slot传递组件模板给组件进行渲染。
-[React]React没有插槽的概念，但其实在组件内部中不论是JSX还是DOM 都会被传入到Props的children当中去，隐含的实现了类似于Vue的普通插槽。
+## 逻辑复用 [React]HOC 与[Vue]mixin
 
-往往我们可能会因为不同的插槽，做不同的事情，比如有一个footer插槽用来改变模态框底部的内容，header用来改变顶部的内容。可以通过传入一个对象的形式进行具名插槽的一个场景模拟。同样的也会将其传递到一个Props当中去。
-```javascript
-<Model>
-{{
-	header: <div>header slot</div>,
-	footer: <div>footer slot</div>
-}}
-</Model>
-```
-
-
-#### [React]HOC 与[Vue]mixin
-> 如何在多个组件之间共享代码是一个重要问题，在 Vue 中组合不同功能的方式是通过 mixin，而在React中通过 HoC (高阶组件）
+> 如何在多个组件之间共享代码是一个重要问题，在 Vue 中组合不同功能的方式是通过 mixin，而在 React 中通过 HoC (高阶组件）
 
 React 最早也是使用 mixins 的，不过后来他们觉得这种方式对组件侵入太强会导致很多问题([由于声明式渲染和自上而下的数据流，许多团队在采用 React 时能够在发布新功能的同时修复一堆错误](https://reactjs.bootcss.com/blog/2016/07/13/mixins-considered-harmful.html))
 
-#### CSS
+## 特殊的
+
+### [Vue]插槽
+
+[vue]在 Vue 中通过 slot 传递组件模板给组件进行渲染。
+[React]React 没有插槽的概念，但其实在组件内部中不论是 JSX 还是 DOM 都会被传入到 Props 的 children 当中去，隐含的实现了类似于 Vue 的普通插槽。
+
+往往我们可能会因为不同的插槽，做不同的事情，比如有一个 footer 插槽用来改变模态框底部的内容，header 用来改变顶部的内容。可以通过传入一个对象的形式进行具名插槽的一个场景模拟。同样的也会将其传递到一个 Props 当中去。
+
+```javascript
+<Model>
+    {{
+        header: <div>header slot</div>,
+        footer: <div>footer slot</div>,
+    }}
+</Model>
+```
+
+### CSS
 
 网页开发有一个原则，叫做[关注点分离（separation of concerns）](https://en.wikipedia.org/wiki/Separation_of_concerns#HTML.2C_CSS.2C_JavaScript)，旨在各种技术只负责自己的领域，不要混合在一起，形成耦合。对于网页开发来说，主要是三种技术分离
-![avatar](./images/README(1).jpg)
-
+![avatar](<./images/README(1).jpg>)
 
 ```javascript
 [bad]
@@ -171,31 +188,33 @@ Hello World
 </h1>
 ```
 
+[vue]在 style 标签上声明一个 scoped 以区分组件样式。最后组件打包时加入一个 hash 值
+[react]使用的 cssModule 方案来进行
 
-[vue]在style标签上声明一个scoped以区分组件样式。最后组件打包时加入一个hash值
-[react]使用的cssModule方案来进行
-- [Bem](https://juejin.cn/post/6844903672162304013) CssName
-- [Style Component](https://juejin.cn/post/6844903878580764686#heading-6)
-  ```javascript
-    import React from 'react';
-    import Styled from 'styled-components'
+-   [Bem](https://juejin.cn/post/6844903672162304013) CssName
+-   [Style Component](https://juejin.cn/post/6844903878580764686#heading-6)
+
+    ```javascript
+    import React from "react";
+    import Styled from "styled-components";
 
     export default () => {
         const Wrapper = Styled.div`
-            & >.title { 
-                color: red;
-            }
-        `
+              & >.title { 
+                  color: red;
+              }
+          `;
         return (
-            <Wrapper>Red Text Wrapper....
+            <Wrapper>
+                Red Text Wrapper....
                 <p className="title">1111</p>
             </Wrapper>
         );
-    }
-  ```
-- CssModule
-- [Css in js](https://www.ruanyifeng.com/blog/2017/04/css_in_js.html)
+    };
+    ```
 
+-   CssModule
+-   [Css in js](https://www.ruanyifeng.com/blog/2017/04/css_in_js.html)
 
 react：umi+dva+antd vue: vue-cli+vuex+element
 
